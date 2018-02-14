4c5cbd0dfdda038a3a6b3d60b7196d81f03fbc4d
git config --global user.email "linkaikai28@126.com <global user.name "lsk567>
2018-02-14 16:21:29 -0500

    create airdrop contract

---
+++ contracts/Airdrop.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.4.18;
+
+import './GainfyToken.sol';
+
+contract Airdrop is GainfyToken {
+
+    function airdrop() public onlyOwner {
+
+        //use a for loop to distribute claimable tokens
+        
+    }
+}
--- contracts/GainfyToken.sol
+++ contracts/GainfyToken.sol
@@ -1,15 +1,16 @@
 pragma solidity ^0.4.18;
 
 import '../node_modules/zeppelin-solidity/contracts/token/ERC20/StandardToken.sol';
+import '../node_modules/zeppelin-solidity/contracts/ownership/Ownable.sol';
 
-contract GainfyToken is StandardToken {
+contract GainfyToken is StandardToken, Ownable {
 
     //declare all the metadata
 	string public constant name = "Gainfy Token";
 	string public constant symbol = "GAIN";
 	uint8 public constant decimals = 2;
 
-    function GainfyToken(uint256 initSupply) {
+    function GainfyToken(uint256 initSupply) Ownable() {
         totalSupply_ = initSupply;
         balances[msg.sender] = totalSupply_;
     }
